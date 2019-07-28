d0691dad17d71f34d99bffa0043eb0f363a051bc
Akshay Sharma <akshaysharma1801@gmail.com>
2019-07-29 00:39:42 +0530

    user service

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -5,9 +5,11 @@ import { WebsiteNolayoutComponent } from './components/website-nolayout/website-
 import { Routes, RouterModule } from '@angular/router';
 import { NoLayoutComponent } from './components/no-layout/no-layout.component';
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
-  declarations: [NavbarComponent, WebsiteNolayoutComponent, NoLayoutComponent],
+  declarations: [NavbarComponent, WebsiteNolayoutComponent, NoLayoutComponent, LoginComponent, SignupComponent],
   imports: [
     CommonModule,
     RouterModule,
